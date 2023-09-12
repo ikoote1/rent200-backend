8269f5f4ba03edd233fd4112ef86333a185d8d77
ikoote1 <ikootepreim@gmail.com>
2023-09-13 01:05:47 +0300

    migrating

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_215915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_owners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
+
+  create_table "periods", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.text "location"
+    t.text "description"
+    t.decimal "price"
+    t.boolean "active"
+    t.integer "capacity"
+    t.string "steering"
+    t.bigint "owner_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "family_id", null: false
+    t.bigint "place_id", null: false
+    t.bigint "period_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["family_id"], name: "index_products_on_family_id"
+    t.index ["owner_id"], name: "index_products_on_owner_id"
+    t.index ["period_id"], name: "index_products_on_period_id"
+    t.index ["place_id"], name: "index_products_on_place_id"
+  end
+
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "families"
+  add_foreign_key "products", "owners"
+  add_foreign_key "products", "periods"
+  add_foreign_key "products", "places"
 end
