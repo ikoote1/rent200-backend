dc6dc11565a1b58a671af11207a9a56de8681a2f
ikoote1 <ikootepreim@gmail.com>
2023-09-12 14:58:40 +0300

    creating methods for my tables

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::FamiliesController < ApplicationController
-end
+    before_action :set_family, only: [:show, :update, :destroy]
+  
+    # GET /api/v1/families
+    def index
+      @families = Family.all
+      render json: @families
+    end
+  
+    # GET /api/v1/families/1
+    def show
+      render json: @family
+    end
+  
+    # POST /api/v1/families
+    def create
+      @family = Family.new(family_params)
+  
+      if @family.save
+        render json: @family, status: :created
+      else
+        render json: @family.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /api/v1/families/1
+    def update
+      if @family.update(family_params)
+        render json: @family
+      else
+        render json: @family.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /api/v1/families/1
+    def destroy
+      @family.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_family
+      @family = Family.find(params[:id])
+    end
+  
+    def family_params
+      params.require(:family).permit(:name)
+    end
+  end
+  
